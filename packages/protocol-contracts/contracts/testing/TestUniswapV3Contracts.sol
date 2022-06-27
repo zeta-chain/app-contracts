1bcdcba0ff648e1f67e0fc184e1fb243f11524f7
Andres Aiello <aaiello@gmail.com>
2022-06-27 14:52:43 -0300

    import uniswap v3 contracts

---
+++ packages/protocol-contracts/contracts/testing/TestUniswapV3Contracts.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.5;
+
+/**
+ * @dev Contracts that need to be compiled for testing purposes
+ */
+
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IPoolInitializer.sol";
