f8f5c2574e10a3970aabccfcf626aaa50abc80ac
Lucas Janon <ljanon@gmail.com>
2022-06-28 20:20:13 -0300

    Rename

--- packages/example-contracts/lib/shared/network.constants.ts
+++ packages/example-contracts/lib/shared/network.constants.ts
@@ -3,64 +3,64 @@ import { NetworkName } from "@zetachain/addresses";
 type ChainId = 0 | 5 | 97 | 1337;
 
 export type NetworkVariables = {
-  CONNECTOR_ADDRESS: string;
   chainId: ChainId;
+  connectorAddress: string;
   crossChainId: ChainId;
   crossChainName: NetworkName | "";
 };
 
 export const networkVariables: Record<NetworkName, NetworkVariables> = {
   "bsc-localnet": {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
   "bsc-testnet": {
-    CONNECTOR_ADDRESS: "0xE626402550fB921E4a47c11568F89dF3496fbEF0",
     chainId: 97,
+    connectorAddress: "0xE626402550fB921E4a47c11568F89dF3496fbEF0",
     crossChainId: 5,
     crossChainName: "goerli",
   },
   "eth-localnet": {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
   "eth-mainnet": {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
   goerli: {
-    CONNECTOR_ADDRESS: "0x68Bc806414e743D88436AEB771Be387A55B4df70",
     chainId: 5,
+    connectorAddress: "0x68Bc806414e743D88436AEB771Be387A55B4df70",
     crossChainId: 97,
     crossChainName: "bsc-testnet",
   },
   hardhat: {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
   "polygon-localnet": {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
   "polygon-mumbai": {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
   ropsten: {
-    CONNECTOR_ADDRESS: "",
     chainId: 0,
+    connectorAddress: "",
     crossChainId: 0,
     crossChainName: "",
   },
