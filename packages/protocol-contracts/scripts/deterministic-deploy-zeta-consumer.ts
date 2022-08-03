4e0f2180724c9c2f64cdf90a6eb593eb71ba6827
Andres Martin Aiello <50411235+andresaiello@users.noreply.github.com>
2022-08-03 11:05:29 -0300

    Redeploy example contracts (#35)
    * Redeploy example contracts
    
    * update addr

---
+++ packages/protocol-contracts/scripts/deterministic-deploy-zeta-consumer.ts
@@ -0,0 +1,53 @@
+import { getAddress, isNetworkName, saveAddress } from "@zetachain/addresses";
+import { BigNumber } from "ethers";
+import { ethers, network } from "hardhat";
+
+import { ZETA_CONSUMER_SALT_NUMBER } from "../lib/contracts.constants";
+import { deployContractToAddress, saltToHex } from "../lib/ImmutableCreate2Factory/ImmutableCreate2Factory.helpers";
+import { ZetaTokenConsumerUniV2__factory } from "../typechain-types";
+
+const DEPLOYER_ADDRESS = process.env.DEPLOYER_ADDRESS ?? "";
+
+export async function deterministicDeployZetaConsumer() {
+  if (!isNetworkName(network.name)) {
+    throw new Error(`network.name: ${network.name} isn't supported.`);
+  }
+
+  const accounts = await ethers.getSigners();
+  const [signer] = accounts;
+
+  const saltNumber = ZETA_CONSUMER_SALT_NUMBER;
+  const saltStr = BigNumber.from(saltNumber).toHexString();
+
+  const zetaToken = getAddress("zetaToken");
+  const uniswapV2Router02 = getAddress("uniswapV2Router02");
+
+  const immutableCreate2Factory = getAddress("immutableCreate2Factory");
+
+  const salthex = saltToHex(saltStr, DEPLOYER_ADDRESS);
+  const constructorTypes = ["address", "address"];
+  const constructorArgs = [zetaToken, uniswapV2Router02];
+
+  const contractBytecode = ZetaTokenConsumerUniV2__factory.bytecode;
+
+  const { address } = await deployContractToAddress({
+    constructorArgs,
+    constructorTypes,
+    contractBytecode,
+    factoryAddress: immutableCreate2Factory,
+    salt: salthex,
+    signer,
+  });
+
+  saveAddress("zetaTokenConsumerUniV2", address);
+  console.log("Deployed ZetaConsumer. Address:", address);
+}
+
+if (!process.env.EXECUTE_PROGRAMMATICALLY) {
+  deterministicDeployZetaConsumer()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
