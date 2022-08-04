2ba6730c0b13a30b478e76ef103b1ad0cb49a4da
Andres Aiello <aaiello@gmail.com>
2022-08-04 14:40:33 -0300

    deploy connector

--- packages/protocol-contracts/lib/contracts.constants.ts
+++ packages/protocol-contracts/lib/contracts.constants.ts
@@ -7,7 +7,7 @@ export const ZETA_TOKEN_SALT_NUMBER_ETH = "38208";
 export const ZETA_TOKEN_SALT_NUMBER_NON_ETH = "29411";
 
 /// dev: this values should be calculated using get-salt script
-export const ZETA_CONNECTOR_SALT_NUMBER_ETH = "91340";
-export const ZETA_CONNECTOR_SALT_NUMBER_NON_ETH = "3024";
+export const ZETA_CONNECTOR_SALT_NUMBER_ETH = "62538";
+export const ZETA_CONNECTOR_SALT_NUMBER_NON_ETH = "4945";
 
 export const ZETA_CONSUMER_SALT_NUMBER = "0";
