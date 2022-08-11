93f7b0644483f211be7ff547677daa924f7072fb
Andres Aiello <aaiello@gmail.com>
2022-08-11 16:56:23 -0300

    add deployment script

---
+++ packages/example-contracts/scripts/multi-chain-swap/deploy-multi-chain-swap-v3.ts
@@ -0,0 +1,32 @@
+import { getAddress, isNetworkName } from "@zetachain/addresses";
+import { saveAddress } from "@zetachain/addresses-tools";
+import { network } from "hardhat";
+
+import { getMultiChainSwapUniV3 } from "../../lib/multi-chain-swap/MultiChainSwap.helpers";
+
+export async function deployMultiChainSwap() {
+  if (!isNetworkName(network.name) || !network.name) throw new Error("Invalid network name");
+
+  const CONNECTOR = getAddress("connector");
+
+  const ZETA_TOKEN = getAddress("zetaToken");
+
+  const UNI_QUOTER_V3 = getAddress("uniswapV3Quoter");
+
+  const UNI_ROUTER_V3 = getAddress("uniswapV3Router");
+
+  const WETH = getAddress("weth9");
+
+  const multiChainSwapContract = await getMultiChainSwapUniV3({
+    deployParams: [CONNECTOR, ZETA_TOKEN, UNI_ROUTER_V3, UNI_QUOTER_V3, WETH, 500, 3000],
+  });
+
+  saveAddress("multiChainSwap", multiChainSwapContract.address);
+}
+
+if (!process.env.EXECUTE_PROGRAMMATICALLY) {
+  deployMultiChainSwap().catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+}
