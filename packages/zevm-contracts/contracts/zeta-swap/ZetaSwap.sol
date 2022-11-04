c036e13988d8beb4f2e7fc3d8a5cc178b16ee226
Andres Aiello <aaiello@gmail.com>
2022-11-04 13:21:08 -0300

    improve swap

--- packages/zevm-contracts/contracts/zeta-swap/ZetaSwap.sol
+++ packages/zevm-contracts/contracts/zeta-swap/ZetaSwap.sol
@@ -11,15 +11,51 @@ interface ZetaSwapErrors {
     error WrongGasContract();
 
     error NotEnoughToPayGasFee();
+
+    error CantBeIdenticalAddresses();
+
+    error CantBeZeroAddress();
 }
 
 contract ZetaSwap is zContract, ZetaSwapErrors {
     uint16 internal constant MAX_DEADLINE = 200;
 
+    address public zetaToken;
     address public immutable uniswapV2Router;
 
-    constructor(address uniswapV2Router_) {
+    constructor(address zetaToken_, address uniswapV2Router_) {
+        zetaToken = zetaToken_;
         uniswapV2Router = uniswapV2Router_;
+    }
+
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        if (tokenA == tokenB) revert CantBeIdenticalAddresses();
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        if (token0 == address(0)) revert CantBeZeroAddress();
+    }
+
+    // calculates the CREATE2 address for a pair without making any external calls
+    function uniswapv2PairFor(
+        address factory,
+        address tokenA,
+        address tokenB
+    ) public pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            hex"ff",
+                            factory,
+                            keccak256(abi.encodePacked(token0, token1)),
+                            hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
+                        )
+                    )
+                )
+            )
+        );
     }
 
     function encode(
@@ -53,9 +89,21 @@ contract ZetaSwap is zContract, ZetaSwapErrors {
             message,
             (address, bytes32, uint256)
         );
-        address[] memory path = new address[](2);
-        path[0] = zrc4;
-        path[1] = targetZRC4;
+
+        address uniswapPool = uniswapv2PairFor(uniswapV2Router, zrc4, targetZRC4);
+        bool existsPairPool = IZRC4(zrc4).balanceOf(uniswapPool) > 0 && IZRC4(zrc4).balanceOf(zetaToken) > 0;
+
+        address[] memory path;
+        if (existsPairPool) {
+            path = new address[](2);
+            path[0] = zrc4;
+            path[1] = targetZRC4;
+        } else {
+            path = new address[](3);
+            path[0] = zrc4;
+            path[1] = zetaToken;
+            path[2] = targetZRC4;
+        }
 
         IZRC4(zrc4).approve(address(uniswapV2Router), amount);
         uint256[] memory amounts = IUniswapV2Router01(uniswapV2Router).swapExactTokensForTokens(
