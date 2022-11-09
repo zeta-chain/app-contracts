18814002945e1ff6e1e2c1961eae485fbd1ab500
Andres Aiello <aaiello@gmail.com>
2022-11-09 15:15:11 -0300

    params to swap

--- packages/zevm-contracts/scripts/zeta-swap/swap.ts
+++ packages/zevm-contracts/scripts/zeta-swap/swap.ts
@@ -1,12 +1,17 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import { parseEther } from "@ethersproject/units";
-import { getAddress } from "@zetachain/addresses";
+import { getAddress, isNetworkName} from "@zetachain/addresses";
 import { ethers } from "hardhat";
 
 import { ZRC20Addresses, TSS_ATHENS2 } from "../systemConstants";
 import { getSwapData } from "./helpers";
+import { network } from "hardhat";
 
 const main = async () => {
+  if (!isNetworkName(network.name) || !network.name) throw new Error("Invalid network name");
+
+  const destinationToken = network.name == 'goerli' ? ZRC20Addresses['tMATIC'] : ZRC20Addresses['gETH']
+
   console.log(`Swapping native token...`);
 
   const [signer] = await ethers.getSigners();
@@ -17,7 +22,7 @@ const main = async () => {
     zetaNetwork: "athens"
   });
 
-  const data = getSwapData(zetaSwap, signer.address, ZRC20Addresses['tMATIC'], BigNumber.from("0"));
+  const data = getSwapData(zetaSwap, signer.address, destinationToken, BigNumber.from("0"));
 
   const tx = await signer.sendTransaction({
     data,
