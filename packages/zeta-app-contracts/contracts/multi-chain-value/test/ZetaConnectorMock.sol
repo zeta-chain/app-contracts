d55da3f9f77fc6e7081c66b5db8fcb5b6354dfc5
Andres Aiello <aaiello@gmail.com>
2023-05-18 12:32:15 -0300

    update zeta-app pkg to use protocol repo

--- packages/zeta-app-contracts/contracts/multi-chain-value/test/ZetaConnectorMock.sol
+++ packages/zeta-app-contracts/contracts/multi-chain-value/test/ZetaConnectorMock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "@zetachain/protocol-contracts/contracts/interfaces/ZetaInterfaces.sol";
+import "@zetachain/protocol-contracts/contracts/evm/interfaces/ZetaInterfaces.sol";
 
 contract ZetaConnectorMockValue is ZetaConnector {
     function send(ZetaInterfaces.SendInput calldata input) external override {}
