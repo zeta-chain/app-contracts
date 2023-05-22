9aebe382aa6a36b2e90e311a39f401d9ef3e063b
Andres Martin Aiello <50411235+andresaiello@users.noreply.github.com>
2023-05-22 11:45:18 -0300

    Fix ZRC20 protocol bug (#94)
    * Fix ZRC20 protocol bug
    
    * rebase fix

--- packages/zevm-example-contracts/contracts/zeta-swap/ZetaSwapBtcInbound.sol
+++ packages/zevm-example-contracts/contracts/zeta-swap/ZetaSwapBtcInbound.sol
@@ -1,40 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "./ZetaSwap.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
+import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
 
-contract ZetaSwapBtcInbound is ZetaSwap {
-    address immutable systemContractAddress;
+import "../shared/BytesHelperLib.sol";
+import "../shared/SwapHelperLib.sol";
 
-    constructor(
-        address zetaToken_,
-        address uniswapV2Factory_,
-        address uniswapV2Router_,
-        address systemContractAddress_
-    ) ZetaSwap(zetaToken_, uniswapV2Factory_, uniswapV2Router_) {
-        systemContractAddress = systemContractAddress_;
+contract ZetaSwapBtcInbound {
+    SystemContract public immutable systemContract;
+
+    constructor(address systemContractAddress) {
+        systemContract = SystemContract(systemContractAddress);
     }
 
-    function bytesToAddress(bytes calldata data, uint256 offset, uint256 size) private pure returns (address output) {
-        bytes memory b = data[offset:offset + size];
-        assembly {
-            output := mload(add(b, size))
-        }
-    }
+    function onCrossChainCall(address zrc20, uint256 amount, bytes calldata message) external {
+        address receipient = BytesHelperLib.bytesToAddress(message, 0);
+        uint32 targetZRC20ChainId = BytesHelperLib.bytesToUint32(message, 20);
+        address targetZRC20 = systemContract.gasCoinZRC20ByChainId(targetZRC20ChainId);
 
-    function bytesToUint32(bytes calldata data, uint256 offset, uint256 size) private pure returns (uint32 output) {
-        bytes memory b = data[offset:offset + size];
-        assembly {
-            output := mload(add(b, size))
-        }
-    }
-
-    function onCrossChainCall(address zrc20, uint256 amount, bytes calldata message) external override {
-        address receipient = bytesToAddress(message, 0, 20);
-        uint32 targetZRC20ChainId = bytesToUint32(message, 20, 4);
-        address targetZRC20 = SystemContract(systemContractAddress).gasCoinZRC20ByChainId(targetZRC20ChainId);
-
-        _doSwap(zrc20, amount, targetZRC20, bytes32(uint256(uint160(receipient))), 0);
+        uint256 outputAmount = SwapHelperLib._doSwap(
+            systemContract.wZetaContractAddress(),
+            systemContract.uniswapv2FactoryAddress(),
+            systemContract.uniswapv2Router02Address(),
+            zrc20,
+            amount,
+            targetZRC20,
+            0
+        );
+        SwapHelperLib._doWithdrawal(targetZRC20, outputAmount, BytesHelperLib.addressToBytes(receipient));
     }
 }
