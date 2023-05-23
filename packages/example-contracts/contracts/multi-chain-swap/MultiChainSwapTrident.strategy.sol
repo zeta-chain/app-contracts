5d39fa51abedbb471864dd2fbc00904082502a1d
Andres Aiello <aaiello@gmail.com>
2023-05-23 11:55:35 -0300

    Remove ZetaTokenConsumerTrident to zetachain repo

--- packages/example-contracts/contracts/multi-chain-swap/MultiChainSwapTrident.strategy.sol
+++ packages/example-contracts/contracts/multi-chain-swap/MultiChainSwapTrident.strategy.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@zetachain/protocol-contracts-deprecated/contracts/ZetaTokenConsumerTrident.strategy.sol";
+import "@zetachain/protocol-contracts/contracts/evm/tools/ZetaTokenConsumerTrident.strategy.sol";
 
 import "./MultiChainSwapErrors.sol";
 import "./MultiChainSwap.sol";
