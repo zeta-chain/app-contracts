9c544189d259457ae650e054ac3b4fe08bddf537
Andres Aiello <aaiello@gmail.com>
2023-10-05 19:56:49 -0300

    rename signer

--- packages/zevm-app-contracts/scripts/cryptopedia-test/mock-stress-swap.ts
+++ packages/zevm-app-contracts/scripts/cryptopedia-test/mock-stress-swap.ts
@@ -16,18 +16,18 @@ const tokenAddress2 = "0x0278d356b23366037F8D3C34e3B387c971197EA2";
 const uniswapRouterAddress = "0x2ca7d64A7EFE2D62A725E2B35Cf7230D6677FfEe";
 
 const stressSwap = async () => {
-  const [deployer] = await ethers.getSigners();
+  const [signer] = await ethers.getSigners();
 
-  const MockToken1 = await MockZETA__factory.connect(tokenAddress1, deployer);
-  const MockToken2 = await MockZETA__factory.connect(tokenAddress2, deployer);
+  const MockToken1 = await MockZETA__factory.connect(tokenAddress1, signer);
+  const MockToken2 = await MockZETA__factory.connect(tokenAddress2, signer);
 
-  await MockToken1.mint(deployer.address, TOKEN_TO_MINT);
+  await MockToken1.mint(signer.address, TOKEN_TO_MINT);
 
-  const uniswapRouter = await UniswapV2Router02__factory.connect(uniswapRouterAddress, deployer);
+  const uniswapRouter = await UniswapV2Router02__factory.connect(uniswapRouterAddress, signer);
 
-  const tokenContract1 = ERC20__factory.connect(tokenAddress1, deployer);
+  const tokenContract1 = ERC20__factory.connect(tokenAddress1, signer);
 
-  const tokenContract2 = ERC20__factory.connect(tokenAddress2, deployer);
+  const tokenContract2 = ERC20__factory.connect(tokenAddress2, signer);
 
   const tx1 = await tokenContract1.approve(uniswapRouter.address, MaxUint256);
   await tx1.wait();
@@ -35,8 +35,8 @@ const stressSwap = async () => {
   const tx2 = await tokenContract2.approve(uniswapRouter.address, MaxUint256);
   await tx2.wait();
 
-  const initialBalance1 = await tokenContract1.balanceOf(deployer.address);
-  const initialBalance2 = await tokenContract2.balanceOf(deployer.address);
+  const initialBalance1 = await tokenContract1.balanceOf(signer.address);
+  const initialBalance2 = await tokenContract2.balanceOf(signer.address);
   console.log(`initialBalance1: ${initialBalance1.toString()}`);
   console.log(`initialBalance2: ${initialBalance2.toString()}`);
 
@@ -44,14 +44,14 @@ const stressSwap = async () => {
     TOKEN_TO_MINT,
     0,
     [tokenAddress1, tokenAddress2],
-    deployer.address,
+    signer.address,
     (await getNow()) + 360,
     { gasLimit: 10_000_000 }
   );
   await tx3.wait();
 
-  const balanceAfterSwap1 = await tokenContract1.balanceOf(deployer.address);
-  const balanceAfterSwap2 = await tokenContract2.balanceOf(deployer.address);
+  const balanceAfterSwap1 = await tokenContract1.balanceOf(signer.address);
+  const balanceAfterSwap2 = await tokenContract2.balanceOf(signer.address);
   console.log(`balanceAfterSwap1: ${balanceAfterSwap1.toString()}`);
   console.log(`balanceAfterSwap2: ${balanceAfterSwap2.toString()}`);
 
@@ -59,23 +59,23 @@ const stressSwap = async () => {
     balanceAfterSwap2.sub(initialBalance2),
     0,
     [tokenAddress2, tokenAddress1],
-    deployer.address,
+    signer.address,
     (await getNow()) + 360,
     { gasLimit: 10_000_000 }
   );
 
   await tx4.wait();
 
-  const balanceAfterSecondSwap1 = await tokenContract1.balanceOf(deployer.address);
-  const balanceAfterSecondSwap2 = await tokenContract2.balanceOf(deployer.address);
+  const balanceAfterSecondSwap1 = await tokenContract1.balanceOf(signer.address);
+  const balanceAfterSecondSwap2 = await tokenContract2.balanceOf(signer.address);
   console.log(`balanceAfterSecondSwap1: ${balanceAfterSecondSwap1.toString()}`);
   console.log(`balanceAfterSecondSwap2: ${balanceAfterSecondSwap2.toString()}`);
 
-  await MockToken1.burn(deployer.address, balanceAfterSecondSwap1);
-  await MockToken2.burn(deployer.address, balanceAfterSecondSwap2);
+  await MockToken1.burn(signer.address, balanceAfterSecondSwap1);
+  await MockToken2.burn(signer.address, balanceAfterSecondSwap2);
 
-  const endBalance1 = await tokenContract1.balanceOf(deployer.address);
-  const endBalance2 = await tokenContract2.balanceOf(deployer.address);
+  const endBalance1 = await tokenContract1.balanceOf(signer.address);
+  const endBalance2 = await tokenContract2.balanceOf(signer.address);
   console.log(`endBalance1: ${endBalance1.toString()}`);
   console.log(`endBalance2: ${endBalance2.toString()}`);
 };
