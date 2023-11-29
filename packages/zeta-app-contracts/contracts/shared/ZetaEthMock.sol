68d5f18d34c6107fb660674f9a5652415ec6d056
Andres Aiello <aaiello@gmail.com>
2023-11-29 11:45:48 -0300

    Update zetachain repo to support protocol 3.0.1

--- packages/zeta-app-contracts/contracts/shared/ZetaEthMock.sol
+++ packages/zeta-app-contracts/contracts/shared/ZetaEthMock.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
 contract ZetaEthMock is ERC20("Zeta", "ZETA") {
-    constructor(uint256 initialSupply) {
-        _mint(msg.sender, initialSupply * (10**uint256(decimals())));
+    constructor(address creator, uint256 initialSupply) {
+        _mint(creator, initialSupply * (10 ** uint256(decimals())));
     }
 }
