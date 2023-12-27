10a6a5bd90092cf658f3d4331da2f160ef46dc3d
Andres Aiello <aaiello@gmail.com>
2023-12-27 17:52:31 -0300

    feat: Iimprove token incentive scripts

---
+++ packages/zevm-app-contracts/scripts/liquidity-incentives/deploy-zrc20-reward.ts
@@ -0,0 +1,39 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { isProtocolNetworkName } from "@zetachain/protocol-contracts";
+import { parseEther } from "ethers/lib/utils";
+import { ethers, network } from "hardhat";
+
+import { RewardDistributorFactory__factory, SystemContract__factory } from "../../typechain-types";
+import { getSystemContractAddress, getZEVMAppAddress } from "../address.helpers";
+import { addReward, deployRewardByNetwork } from "./helpers";
+
+const REWARD_DURATION = BigNumber.from("604800").mul(8); // 1 week * 8
+const REWARDS_AMOUNT = parseEther("500");
+const NETWORK_NAME = "goerli_testnet"; // @dev: change to your network name
+
+const main = async () => {
+  const [deployer] = await ethers.getSigners();
+  const networkName = network.name;
+
+  if (!isProtocolNetworkName(networkName)) throw new Error("Invalid network name");
+
+  const systemContractAddress = getSystemContractAddress();
+  const systemContract = await SystemContract__factory.connect(systemContractAddress, deployer);
+
+  const factoryContractAddress = getZEVMAppAddress("rewardDistributorFactory");
+
+  const rewardDistributorFactory = RewardDistributorFactory__factory.connect(factoryContractAddress, deployer);
+
+  const rewardContractAddress = await deployRewardByNetwork(
+    deployer,
+    systemContract,
+    NETWORK_NAME,
+    rewardDistributorFactory
+  );
+  await addReward(deployer, systemContract, rewardContractAddress, REWARD_DURATION, REWARDS_AMOUNT);
+};
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
