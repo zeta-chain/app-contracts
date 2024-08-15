82aafaf7c6e7156ba84749ac5e8d4da8ad677a14
Andres Aiello <aaiello@gmail.com>
2024-08-15 14:11:04 -0300

    deploy and verify

--- packages/zevm-app-contracts/scripts/xp-nft/deploy.ts
+++ packages/zevm-app-contracts/scripts/xp-nft/deploy.ts
@@ -1,5 +1,5 @@
 import { isProtocolNetworkName } from "@zetachain/protocol-contracts";
-import { ethers, network, upgrades } from "hardhat";
+import { ethers, network, run, upgrades } from "hardhat";
 
 import { ZetaXP__factory } from "../../typechain-types";
 import { saveAddress } from "../address.helpers";
@@ -9,6 +9,20 @@ const networkName = network.name;
 const ZETA_BASE_URL = "https://api.zetachain.io/nft/";
 const signer = "0x1d24d94520B94B26351f6573de5ef9731c48531A";
 
+const verifyContract = async (contractAddress: string, constructorArguments: any[]) => {
+  // Verification process
+  console.log(`Verifying contract ${contractAddress}...`);
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments,
+    });
+    console.log("Verification successful");
+  } catch (error) {
+    console.error("Verification failed:", error);
+  }
+};
+
 const deployZetaXP = async () => {
   if (!isProtocolNetworkName(networkName)) throw new Error("Invalid network name");
 
@@ -17,8 +31,16 @@ const deployZetaXP = async () => {
 
   await zetaXP.deployed();
 
+  // Get the implementation address
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(zetaXP.address);
+
   console.log("ZetaXP deployed to:", zetaXP.address);
+  console.log("ZetaXP implementation deployed to:", implementationAddress);
+
   saveAddress("ZetaXP", zetaXP.address, networkName);
+
+  await verifyContract(zetaXP.address, ["ZETA NFT", "ZNFT", ZETA_BASE_URL, signer]);
+  await verifyContract(implementationAddress, []);
 };
 
 const main = async () => {
