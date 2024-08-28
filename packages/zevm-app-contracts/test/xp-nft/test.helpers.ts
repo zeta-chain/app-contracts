db688da1be81d2937372eaa1526c7b3ab5a4f142
Andres Martin Aiello <50411235+andresaiello@users.noreply.github.com>
2024-08-28 15:18:30 -0300

    feat: XP NFT contract audit suggestions (#175)

--- packages/zevm-app-contracts/test/xp-nft/test.helpers.ts
+++ packages/zevm-app-contracts/test/xp-nft/test.helpers.ts
@@ -1,39 +1,49 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { ethers } from "hardhat";
-
-export interface Signature {
-  r: string;
-  s: string;
-  v: number;
-}
 
 export interface NFT {
   tag: string;
   to: string;
+  tokenId: number | undefined;
 }
 
-export interface UpdateParam extends NFT {
+export interface NFTSigned extends NFT {
   sigTimestamp: number;
-  signature: Signature;
+  signature: string;
   signatureExpiration: number;
-  tokenId: number;
 }
 
 export const getSignature = async (
+  chainId: number,
+  verifyingContract: string,
   signer: SignerWithAddress,
   signatureExpiration: number,
   timestamp: number,
   to: string,
   nft: NFT
 ) => {
-  let payload = ethers.utils.defaultAbiCoder.encode(
-    ["address", "uint256", "uint256", "bytes32"],
-    [to, signatureExpiration, timestamp, nft.tag]
-  );
+  const domain = {
+    chainId: chainId,
+    name: "ZetaXP",
+    verifyingContract: verifyingContract,
+    version: "1",
+  };
 
-  const payloadHash = ethers.utils.keccak256(payload);
+  const types = {
+    MintOrUpdateNFT: [
+      { name: "to", type: "address" },
+      { name: "signatureExpiration", type: "uint256" },
+      { name: "sigTimestamp", type: "uint256" },
+      { name: "tag", type: "bytes32" },
+    ],
+  };
 
-  // This adds the message prefix
-  const signature = await signer.signMessage(ethers.utils.arrayify(payloadHash));
-  return ethers.utils.splitSignature(signature);
+  const value = {
+    sigTimestamp: timestamp,
+    signatureExpiration,
+    tag: nft.tag,
+    to,
+  };
+  // Signing the data
+  const signature = await signer._signTypedData(domain, types, value);
+  return signature;
 };
